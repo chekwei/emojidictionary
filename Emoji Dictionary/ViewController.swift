8586688a976cc2243aa18f6bd7707716023aa056
Chia Chekwei <chekwei@me.com>
2019-03-18 20:35:13 +0800

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Chia Chekwei on 18/3/19.
+//  Copyright © 2019 Chekwei Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
